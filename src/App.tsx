1686198da0f2909d6bf6267e5c60c47da79433a4
tiago <trsminfo@gmail.com>
2025-05-23 17:26:00 -0300

    component de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Container } from "./components/Container";
+import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 
@@ -7,14 +9,40 @@ export function App() {
   return (
     <>
       <Container>
-
         <Logo />
-
-
       </Container>
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput id="input"
+              type="text"
+              labelText="Tarefas"
+              placeholder="Digite uma tarefa" 
+             
+              />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 0</p>
+          </div>
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
 
     </>
---
+++ src/components/DefaultInput/index.tsx
@@ -0,0 +1,17 @@
+import styles from './style.module.css';
+
+type DefaultInputProps = {
+    id: string;
+    labelText: string;
+} & React.ComponentProps<'input'>
+
+
+export function DefaultInput({ id, labelText, type, ...rest }: DefaultInputProps) {
+
+    return (
+        <>
+            <label htmlFor={id}>{labelText}</label>
+            <input className={styles.input}  type={type} id={id} {...rest} />
+        </>
+    )
+}
